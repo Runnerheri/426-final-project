750ab075c88d5325bc0edb79ccb8966b8753510a
kiarajg <kiarajg@ad.unc.edu>
2024-04-27 06:01:33 -0400

    Frontend development

---
+++ frontend/src/Components/Income/Income.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components';
+import { useGlobalContext } from '../../context/globalContext';
+import { InnerLayout } from '../../styles/layouts';
+
+function Income() {
+    const {addIncome} = useGlobalContext()
+    return (
+        <IncomeStyled>
+            <InnerLayout>
+                <h1>Incomes</h1>
+                <div className='income-content'>
+                    <div className='form-container'>
+                    </div> 
+                    <div className='incomes'>
+
+                    </div>
+                </div>
+            </InnerLayout>
+        </IncomeStyled>
+    )
+}
+
+const IncomeStyled = styled.div`
+
+`;
+
+export default Income
