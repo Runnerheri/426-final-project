1af0d22595c6e80d35c112e55698607760704c24
Arizona Palmeri <84550659+BoracicNards@users.noreply.github.com>
2024-04-29 06:18:16 -0400

    income component

--- frontend/src/Components/Form/Form.js
+++ frontend/src/Components/Form/Form.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 import styled from 'styled-components'
 import DatePicker from 'react-datepicker'
@@ -5,6 +6,19 @@ import 'react-datepicker/dist/react-datepicker.css'
 
 
 function Form() {
+=======
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
+import { useGlobalContext } from '../../context/globalContext';
+import Button from '../Button/Button';
+import { plus } from '../../utils/Icons';
+
+
+function Form() {
+    const {addIncome, getIncomes, error, setError} = useGlobalContext()
+>>>>>>> Stashed changes
     const [inputState, setInputState] = useState({
         title: '',
         amount: '',
@@ -13,12 +27,14 @@ function Form() {
         description: '',
     })
 
-    const {title, amount, date, category, description } = inputState;
+    const { title, amount, date, category,description } = inputState;
 
     const handleInput = name => e => {
         setInputState({...inputState, [name]: e.target.value})
+        setError('')
     }
 
+<<<<<<< Updated upstream
     return (
         <FormStyled>
             <div className='input-control'>
@@ -29,27 +45,49 @@ function Form() {
                     placeholder="Salary Title"
                     onchange={handleInput('title')}
 
-                />
-            </div>
-            <div className='input-control'>
-                <input value={amount}
-                    type='text'
-                    name={'amount'}
-                    placeholder="Salary Amount"
-                    onchange={handleInput('amount')}
+=======
+    const handleSubmit = e => {
+        e.preventDefault()
+        addIncome(inputState)
+        setInputState({
+            title: '',
+            amount: '',
+            date: '',
+            category: '',
+            description: '',
+        })
+    }
 
+    return (
+        <FormStyled onSubmit={handleSubmit}>
+            {error && <p className='error'>{error}</p>}
+            <div className="input-control">
+                <input 
+                    type="text" 
+                    value={title}
+                    name={'title'} 
+                    placeholder="Salary Title"
+                    onChange={handleInput('title')}
+>>>>>>> Stashed changes
                 />
             </div>
-            <div className='input-control'>
+            <div className="input-control">
+                <input value={amount}  
+                    type="text" 
+                    name={'amount'} 
+                    placeholder={'Salary Amount'}
+                    onChange={handleInput('amount')} 
+                />
+            </div>
+            <div className="input-control">
                 <DatePicker 
                     id='date'
                     placeholderText='Enter A Date'
                     selected={date}
-                    dateFormat='dd/MM/yyyy'
+                    dateFormat="dd/MM/yyyy"
                     onChange={(date) => {
                         setInputState({...inputState, date: date})
                     }}
-                
                 />
             </div>
             <div className="selects input-control">
@@ -65,11 +103,75 @@ function Form() {
                     <option value="other">Other</option>  
                 </select>
             </div>
+<<<<<<< Updated upstream
+=======
+            <div className="input-control">
+                <textarea name="description" value={description} placeholder='Add A Reference' id="description" cols="30" rows="4" onChange={handleInput('description')}></textarea>
+            </div>
+            <div className="submit-btn">
+                <Button 
+                    name={'Add Income'}
+                    icon={plus}
+                    bPad={'.8rem 1.6rem'}
+                    bRad={'30px'}
+                    bg={'var(--color-accent'}
+                    color={'#fff'}
+                />
+            </div>
+>>>>>>> Stashed changes
         </FormStyled>
     )
 }
 
-const FormStyled = styled.form`
 
+const FormStyled = styled.form`
+<<<<<<< Updated upstream
+
+=======
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    input, textarea, select{
+        font-family: inherit;
+        font-size: inherit;
+        outline: none;
+        border: none;
+        padding: .5rem 1rem;
+        border-radius: 5px;
+        border: 2px solid #fff;
+        background: transparent;
+        resize: none;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        color: rgba(34, 34, 96, 0.9);
+        &::placeholder{
+            color: rgba(34, 34, 96, 0.4);
+        }
+    }
+    .input-control{
+        input{
+            width: 100%;
+        }
+    }
+
+    .selects{
+        display: flex;
+        justify-content: flex-end;
+        select{
+            color: rgba(34, 34, 96, 0.4);
+            &:focus, &:active{
+                color: rgba(34, 34, 96, 1);
+            }
+        }
+    }
+
+    .submit-btn{
+        button{
+            box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+            &:hover{
+                background: var(--color-green) !important;
+            }
+        }
+    }
+>>>>>>> Stashed changes
 `;
 export default Form
